9a3c17189f8869c267ecc6214712fdbdb863c5a9
llyhuayan <55656476+llyhuayan@users.noreply.github.com>
2019-11-30 22:53:29 +0800

    Update app.py

--- simpledu/simpledu/app.py
+++ simpledu/simpledu/app.py
@@ -3,15 +3,16 @@ from flask import Flask, render_template
 from simpledu.config import configs
 from simpledu.models import db, Course
 
-# 工厂函数
+
 def create_app(config):
-    #可以根据传入的config名称，加载不同的配置"""
+    """ 可以根据传入的 config 名称，加载不同的配置
+    """
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
-    # SQLALchemy 的初始化方式改为使用init_app
+    # SQLAlchemy 的初始化方式改为使用 init_app
     db.init_app(app)
 
-    #路由函数暂时写在这里，
+    # 路由函数暂时写在这里，后面会介绍使用 Flask 的 Blueprint 实现
     # 路由的模块化
     @app.route('/')
     def index():
@@ -23,5 +24,3 @@ def create_app(config):
         return 'admin'
 
     return app
-
-
