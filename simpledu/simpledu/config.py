48d0d17daff3051350270b21bf006e697af8d15f
llyhuayan <55656476+llyhuayan@users.noreply.github.com>
2019-11-30 22:51:37 +0800

    Update config.py

--- simpledu/simpledu/config.py
+++ simpledu/simpledu/config.py
@@ -1,23 +1,22 @@
 #存放配置，待用
 
 class BaseConfig(object):
-#"""配置基类"""
+    """ 配置基类 """
     SECRET_KEY = 'makesure to set a very secret key'
 
-
 class DevelopmentConfig(BaseConfig):
-#"""开发环境配置"""
+    """ 开发环境配置 """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
 
 
 class ProductionConfig(BaseConfig):
-#"""生产环境配置"""
+    """ 生产环境配置 """
     pass
 
 
 class TestingConfig(BaseConfig):
-#"""测试环境配置"""
+    """ 测试环境配置 """
     pass
 
 
@@ -25,5 +24,4 @@ configs = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
     'testing': TestingConfig
-
 }
